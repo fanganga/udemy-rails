5f07b3387d6f7c8adc65baa21f5bd480e87e2b4a
Andrew <shanks.andy@gmail.com>
2016-01-04 20:36:52 +0000

    Add partials and destroy action. Also linked index from home

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,9 @@
 class ArticlesController < ApplicationController
-  
+
   def index
     @articles = Article.all
   end
-  
+
   def new
     @article = Article.new
   end
@@ -35,7 +35,13 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
-  
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
 
   private def article_params
     params.require(:article).permit(:title, :description)
