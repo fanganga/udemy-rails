fad3ab6b5588840fe84eb9ab9f8d70805ed1128f
Andrew <shanks.andy@gmail.com>
2016-01-18 18:39:58 +0000

    ASsociated user to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
   validates :title,presence: true, length: {minimum: 3, maximum:350}
-  validates :description,presence: true, length: {minimum:3}
+  validates :user_id, presence: true
+  validates :description,presence: true, length: {minimum:3, maximum:10000}
+
 end
 
