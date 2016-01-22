ca11344c73e797aadd0f91ba585dd84e59aaed27
Andrew <shanks.andy@gmail.com>
2016-01-22 21:45:01 +0000

    stores author properly

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
   def create
     debugger
     @article = Article.create(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was created."
        redirect_to article_path(@article)
