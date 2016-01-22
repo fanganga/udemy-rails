7fbd9fc8617423cfcd4451389a87638624684f14
Andrew <shanks.andy@gmail.com>
2016-01-22 14:22:10 +0000

    First attempt - not working.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :find_article, only: [:edit, :show, :update, :destroy]
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page])
   end
 
   def new
