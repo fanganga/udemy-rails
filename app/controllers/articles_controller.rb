658d81a352629d35e138cbe51791277809a415a9
Andrew <shanks.andy@gmail.com>
2016-01-28 16:06:22 +0000

    Attempting to fix problem in heroku

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,14 +3,14 @@ class ArticlesController < ApplicationController
   before_action :find_article, only: [:edit, :show, :update, :destroy]
   before_action :require_user, except: [:index, :show]
   before_action :require_same_user, only: [:edit, :update, :destroy]
-  
+
   def require_same_user
     if current_user != @article.user && !current_user.admin
       flash[:danger] = "You can only edit or delete an article you created"
       redirect_to root_path
     end
   end
-  
+
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
   end
@@ -52,11 +52,13 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
-  private def article_params
+  private 
+  
+  def article_params
     params.require(:article).permit(:title, :description, category_ids:[])
   end
 
-  private def find_article
+ def find_article
     @article = Article.find(params[:id])
   end
 end
